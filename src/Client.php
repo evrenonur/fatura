ca02f53ce60df72a10ac1d0e6202937169df359d
Onur Evren <onur.evren.1999@gmail.com>
2024-12-17 16:30:27 +0300

    Proxy added

--- src/Client.php
+++ src/Client.php
@@ -20,6 +20,47 @@ class Client
     protected static $headers = [
         'content-type' => 'application/x-www-form-urlencoded',
     ];
+
+    /**
+     * @var array proxy configuration
+     */
+    protected static $proxy = [];
+    
+    /**
+     * setProxy
+     * 
+     * @param string $proxy Proxy URL (örn: "http://username:password@proxy.example.com:8080")
+     * @return void
+     */
+    public static function setProxy(string $proxy): void
+    {
+        // URL'den kullanıcı adı ve şifreyi ayıkla
+        $parsedUrl = parse_url($proxy);
+        
+        if (!isset($parsedUrl['scheme'])) {
+            $parsedUrl['scheme'] = 'http';
+        }
+
+        if (isset($parsedUrl['user']) && isset($parsedUrl['pass'])) {
+            self::$proxy = [
+                'proxy' => "{$parsedUrl['scheme']}://{$parsedUrl['user']}:{$parsedUrl['pass']}@{$parsedUrl['host']}:{$parsedUrl['port']}"
+            ];
+        } else {
+            self::$proxy = [
+                'proxy' => $proxy
+            ];
+        }
+    }
+
+    /**
+     * clearProxy
+     * 
+     * @return void
+     */
+    public static function clearProxy(): void
+    {
+        self::$proxy = [];
+    }
     
     /**
      * request
@@ -31,16 +72,31 @@ class Client
     public function __construct(string $url, ?array $parameters = null, bool $post = true)
     {
         try {
-            $request = (new \GuzzleHttp\Client)->request($post ? 'POST' : 'GET', $url, [
-                'headers'     => self::$headers, 
-                'form_params' => $parameters
-            ]);
+            $config = [
+                'verify' => false,
+            ];
+
+            // Proxy ayarlarını config'e ekle
+            if (!empty(self::$proxy)) {
+                $config = array_merge($config, self::$proxy);
+            }
+
+          
+            $client = new \GuzzleHttp\Client($config);
+
+            $options = [
+                'headers'     => self::$headers,
+                'form_params' => $parameters,
+            ];
+
+            $request = $client->request($post ? 'POST' : 'GET', $url, $options);
+            
             if ($response = json_decode($request->getBody()->getContents(), true)) {
                 if (is_array($response)) {
                     $this->response = $response;
                 }
             }
-            //if (!$this->response || (isset($this->response['data']) && !is_array($this->response['data'])) || isset($this->response['error']) || !empty($this->response['data']['hata'])) {
+
             if (!$this->response || isset($this->response['error']) || !empty($this->response['data']['hata'])) {
                 throw new ApiException('İstek başarısız oldu.', $parameters, $this->response, $request->getStatusCode());
             }
